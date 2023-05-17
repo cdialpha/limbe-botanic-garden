4895daec5fb83c73cd759b7d85d1c225f85434f0
cdialpha <cdialpha@gmail.com>
2023-05-17 12:51:55 -0400

    fix experimental options on nextjs.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const repo = "african_women_dmv";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
+  output: "export",
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
